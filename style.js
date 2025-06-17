8fba93379303fa5c969be5406d541179b4c4ad16
Yash Singhal <yash20singhal@gmail.com>
2025-06-17 17:57:49 +0530

    Add files via upload
    Nav & Footer Basic using HTML,CSS and JavaScript

---
+++ style.js
@@ -0,0 +1,9 @@
+function show() {
+    let showButton = document.querySelector('.phoneSize');
+    showButton.style.display = 'flex';
+    
+  }
+  function hide() {
+    let showButton = document.querySelector('.phoneSize');
+    showButton.style.display = 'none'
+  }
